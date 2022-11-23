5356dd4787880a8c172ca612459ba5375453d76f
sumyataung2004 <sumyataung1584@gmail.com>
2022-11-23 18:39:50 +0630

    add review

---
+++ src/components/review/review-data.jsx
@@ -0,0 +1,72 @@
+const data = [
+  {
+    id: 0,
+    name: "Georgia Perez",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpKlYIQ8XzwrIACoNnoR2Hcdwi62t76TiQlQ&usqp=CAU",
+    review:
+      "Great Food, Great Service.Came in here for lunch on a work day. I ordered Kim Bop, and Suntofu Soup. There isn't much to say except that the food was excellent, the sides were delicious, the soup was filling, savory and spicy, the Kim bop was the best I&#39;ve had. It was just a great meal. I will definitely be back soon.",
+  },
+  {
+    id: 1,
+    name: "Journey Shah",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPPiH0zu392nzTLVYkKJbaphcLPWogibD_bA&usqp=CAU",
+    review:
+      "I love this place, this is my favorite restaurant! highly recommend.",
+  },
+  {
+    id: 2,
+    name: "Luis Blake",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYIOkbbkCgNYOOuqjVNx4y-hjP4zzUwP3pUKzYeKaCN9f7U5rEIZx0hFSRKh735j_lkEA&usqp=CAU",
+    review:
+      "This is a really fantastic restaurant, really good food and service.",
+  },
+  {
+    id: 3,
+    name: "Triston Ayala",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXu6cFZw_jab6H-zvZELFnQbWdOkY7rcTSsA&usqp=CAU",
+    review:
+      "Lovely food, 100% recomend, fast delivery and piping hot brilliant",
+  },
+  {
+    id: 4,
+    name: "Gabriela Gross",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhdpc7sd4jVmUxQ1Fnu603JZFIQKyUANlMZg&usqp=CAU",
+    review:
+      "AMAZING, their food is 100% fresh, i ordered 4 dishes, fast delivery and fresh food.",
+  },
+  {
+    id: 5,
+    name: "Eddie Johnson",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREUlL8Y3p8aPL0mhDt2Irz5hNyBIlc4ZuXmw&usqp=CAU",
+    review:
+      "Whenever I wanna eat Translucent's food, I book a dinning but somethings I don't wanna go out, So I order it, fresh and best.",
+  },
+  {
+    id: 6,
+    name: "Antoine Arellano",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT4RiLhXBjmN2PX88mTAPZJfe9j0jl4VaNsw&usqp=CAU",
+    review:
+      "I was patiently waiting to give a review to Translucent. Whoever is running this venture, is going amazing job. I love the taste, quality, hygiene and everything about it. The best thing is i can order 24/7 just by sitting at home.",
+  },
+  {
+    id: 7,
+    name: "Braden Mccoy",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-85zn3gpEj9BjAfSseea424n080SfusWkUA&usqp=CAU",
+    review: "The quality, taste of the food > ",
+  },
+  {
+    id: 8,
+    name: "Braelyn Day",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ5jS6KJQPhlyRkeDuJ5VQiBrtFJDjLjjfag&usqp=CAU",
+    review:
+      "Quick bite or a leisurely meal, I swear Translucent is the perfect choice for anyone who wants great tasting food without having to compromise on quality or healthfulness.",
+  },
+  {
+    id: 9,
+    name: "Patrick Farrell",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzGdFDR8QI1SWwz_IpMiXuuM4KWboph_Z1GQ&usqp=CAU",
+    review: "OMG effordable price for everyone",
+  },
+];
+
+export default data;
--- src/pages/Review.jsx
+++ src/pages/Review.jsx
@@ -1,17 +1,65 @@
 import React from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+import styled from "styled-components";
 
 import Nav from "../components/header/Nav";
+import data from "../components/review/review-data";
 
 const Review = () => {
   return (
-    <>
+    <div className="bg-bla">
       <Nav />
-      Review
-    </>
+      <div className="flex justify-center items-center mx-20 flex-wrap pb-20 ">
+        {data.map((d) => {
+          return (
+            <ReviewStyle
+              key={d.id}
+              className="flex justify-center items-center flex-col mt-20 m-5 "
+            >
+              <div className="flex justify-center items-center flex-col ">
+                <img
+                  src={d.img}
+                  alt="human"
+                  className="rounded-full w-[200px] h-[200px] object-cover"
+                />
+                <h1 className="text-yel text-3xl mt-5">{d.name}</h1>
+
+                <h2 className="text-gray-50 text-sm w-[350px] mt-5 text-center font-mono">
+                  {`" ${d.review} "`}
+                </h2>
+              </div>
+            </ReviewStyle>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
 export default Review;
 
-// Whether you're looking for a
-// quick bite or a leisurely meal, Translucent is the perfect choice for anyone who wants great tasting food without having to compromise on quality or healthfulness.
+// --------------------- //
+
+// styled components
+
+const ReviewStyle = styled.div`
+  div {
+    box-shadow: 0 4px 8px 0 rgba(12, 11, 8, 0.2),
+      0 6px 0px 0 rgba(12, 11, 8, 0.19);
+    background-color: #12100d;
+    padding: 30px;
+
+    @media all and (max-width: 440px) {
+      padding: 30px 10px;
+    }
+
+    @media all and (max-width: 360px) {
+      padding: 30px 2px;
+    }
+  }
+
+  h1 {
+    font-family: "Cormorant Upright", serif;
+  }
+`;
