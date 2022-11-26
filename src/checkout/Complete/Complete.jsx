a79cd7dac475296e594e294959f1a35876fe9091
sumyataung2004 <sumyataung1584@gmail.com>
2022-11-26 23:58:10 +0630

    confirmation-page

--- src/checkout/Complete/Complete.jsx
+++ src/checkout/Complete/Complete.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Complete = () => {
-  return <div className="h-[600px] border">Confirmation</div>;
+  return (
+    <div className="h-[300px] border flex justify-center items-center flex-col">
+      <h1 className="text-3xl font-mono tracking-wide text-gray-800">
+        Thank You
+      </h1>
+      <h1 className="text-xl text-gray-700 mt-5">
+        Your Order was Completed Successfully
+      </h1>
+    </div>
+  );
 };
 
 export default Complete;
